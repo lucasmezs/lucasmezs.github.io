4b18eec4704e7fc35c75bdf88a709160a5220c0a
Lucas Menezes <lucasm@lucasm.dev>
2021-01-25 12:00:54 -0300

    microsoft clarity

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -69,6 +69,17 @@ export default function PageLayout(props) {
 				`,
 			}}
 		></script>
+		<script
+		dangerouslySetInnerHTML={{
+					__html: `
+			(function(c,l,a,r,i,t,y){
+				c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+				t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+				y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+			})(window, document, "clarity", "script", "3j9kmg1bwm");
+			`,
+		}}
+		></script>
     </Head>
 
     <Header/>
